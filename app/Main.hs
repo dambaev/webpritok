c3620023adf4a8d0d33b3e69a9e4c09cedf0eb0d
Dambaev Alexander <ice.redmine@gmail.com>
2017-08-03 15:55:11 +0800

    provide RTS stats

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,54 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
 module Main where
+
+import qualified Web.Scotty as WS
+import qualified Network.HTTP.Types.Status as NHTS
+  ( serviceUnavailable503
+  )
+
+import Control.Monad.IO.Class
+  ( liftIO
+  )
+
+import qualified GHC.Stats as GS
+
+import qualified Data.Aeson.Types as A
 
 import Lib
 
 main :: IO ()
-main = someFunc
+main = WS.scotty 3000 $ do
+  WS.get "/status" $ doStatus
+
+
+instance A.ToJSON GS.GCStats where
+  toJSON (GS.GCStats {..}) = A.object
+    [ "bytesAllocated" A..= bytesAllocated
+    , "numGcs" A..= numGcs
+    , "maxBytesUsed" A..= maxBytesUsed
+    , "numByteUsageSamples" A..= numByteUsageSamples
+    , "cumulativeBytesUsed" A..= cumulativeBytesUsed
+    , "bytesCopied" A..= bytesCopied
+    , "currentBytesUsed" A..= currentBytesUsed
+    , "currentBytesSlop" A..= currentBytesSlop
+    , "maxBytesSlop" A..= maxBytesSlop
+    , "peakMegabytesAllocated" A..= peakMegabytesAllocated
+    , "mutatorCpuSeconds" A..= mutatorCpuSeconds
+    , "mutatorWallSeconds" A..= mutatorWallSeconds
+    , "gcCpuSeconds" A..= gcCpuSeconds
+    , "gcWallSeconds" A..= gcWallSeconds
+    , "cpuSeconds" A..= cpuSeconds
+    , "wallSeconds" A..= wallSeconds
+    , "parTotBytesCopied" A..= parTotBytesCopied
+    , "parMaxBytesCopied" A..= parMaxBytesCopied
+    ]
+
+doStatus:: WS.ActionM ()
+doStatus = do
+  enabled <- liftIO $ GS.getGCStatsEnabled
+  if not enabled
+    then WS.status $ NHTS.serviceUnavailable503
+    else do
+      stats <- liftIO $ GS.getGCStats
+      WS.json $ stats
