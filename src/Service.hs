3cfe352e3886b95a71b1835d147153724465d9f9
Dambaev Alexander <ice.redmine@gmail.com>
2017-08-03 20:05:54 +0800

    temp

--- src/Service.hs
+++ src/Service.hs
@@ -15,6 +15,8 @@ import qualified Language.C.Inline as C
 
 import qualified Web.Scotty as S
 
+C.context (C.baseCtx <> C.bsCtx)
+
 C.include "windows.h"
 C.include "winsvc.h"
 
@@ -23,14 +25,34 @@ isServiceRunning name = do
   ibool <- liftIO $ fmap fromIntegral
     [C.block|int
       {
-        SC_HANDLE h_svc = 0;
-        h_svc = OpenSCManager( 0, 0, GENERIC_READ);
+        char * s_svc_name = 0;
+        size_t sz_svc_name = 0;
+        int ret = 0;
+        sz_svc_name = $bs-len:name;
+        s_svc_name = malloc( sz_svc_name + 1);
+        if( !s_svc_name)
+          return 0;
+        s_svc_name[ sz_svc_name ] = 0;
+        memcpy( s_svc_name, $bs-ptr:name, sz_svc_name);
+        SC_HANDLE h_scm = 0;
+        h_scm = OpenSCManager( 0, 0, GENERIC_READ);
         if( !h_svc)
         {
-          return 0;
+          goto cleanup1;
         }
+        SC_HANDLE h_svc = 0;
+        h_svc = OpenService( h_scm, s_svc_name, GENERIC_READ);
+        if( !h_svc)
+        {
+          goto cleanup2;
+        }
+
+
+        cleanup2:
         CloseServiceHandle( h_svc);
-        return 1;
+        cleanup1:
+        free( s_svc_name);
+        return ret;
       }
               |]
   if ibool == 0
