75ebf6af19321fe80d3eddd0b9e4e0b3c60788ef
Dambaev Alexander <ice.redmine@gmail.com>
2017-08-03 19:38:49 +0800

    initial commit for Service handler

--- app/Main.hs
+++ app/Main.hs
@@ -3,8 +3,9 @@
 module Main where
 
 import qualified Web.Scotty as WS
-import qualified Network.HTTP.Types.Status as NHTS
+import qualified Network.HTTP.Types.Status as HTTP
   ( serviceUnavailable503
+  , ok200
   )
 
 import Control.Monad.IO.Class
@@ -14,14 +15,27 @@ import Control.Monad.IO.Class
 import qualified GHC.Stats as GS
 
 import qualified Data.Aeson.Types as A
+import qualified Network.Wai as Wai
 
-import Lib
+import qualified Data.ByteString.Char8 as C8
+
+import Service
 
 main :: IO ()
 main = WS.scotty 3000 $ do
+  -- maybe, it should be authenticated too?
   WS.get "/status" $ doStatus
+  
+  -- first, authenticate get request
+  WS.get (WS.function matchBadGetAuthenticate) doBadGetAuthenticate
+  WS.get "/service/running/:name" $ doServiceStatus
+  WS.get (WS.function $ \_-> Just []) $ handleGet
+
+  -- post request
 
 
+
+-- GS.GCStats should be representable as JSON data  
 instance A.ToJSON GS.GCStats where
   toJSON (GS.GCStats {..}) = A.object
     [ "bytesAllocated" A..= bytesAllocated
@@ -48,7 +62,44 @@ doStatus:: WS.ActionM ()
 doStatus = do
   enabled <- liftIO $ GS.getGCStatsEnabled
   if not enabled
-    then WS.status $ NHTS.serviceUnavailable503
+    then WS.status $ HTTP.serviceUnavailable503
     else do
       stats <- liftIO $ GS.getGCStats
       WS.json $ stats
+
+handleGet:: WS.ActionM ()
+handleGet = do
+  WS.html "handleGet"
+
+-- | will return Nothing if hash is presents and matches to request
+matchBadGetAuthenticate:: Wai.Request-> Maybe [WS.Param]
+matchBadGetAuthenticate req = Nothing {- let
+--  reqNoHash = getUrlWithoutHash
+  in case mhash of
+       Nothing-> Just []
+       Just hash ->
+         if hash == rehashed
+           then Nothing
+           else Just []
+     where
+       query = Wai.queryString req
+       mhash = case filter (\(key, _) -> key == "hash") query of
+         [] -> Nothing
+         ((_, hash):_) -> case hash of
+                            Nothing -> Nothing
+                            Just some -> Just some
+-}
+                                
+doBadGetAuthenticate:: WS.ActionM ()
+doBadGetAuthenticate = WS.status $ HTTP.serviceUnavailable503
+
+  
+
+-- | checks 
+doServiceStatus:: WS.ActionM ()
+doServiceStatus = do
+  name <- WS.param "name"
+  running <- isServiceRunning name
+  if not running
+    then WS.status $ HTTP.serviceUnavailable503
+    else WS.status $ HTTP.ok200
